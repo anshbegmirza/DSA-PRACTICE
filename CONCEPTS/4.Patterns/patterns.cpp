ac5f8e941f40b8e47740ab49f3c3f713b08cf128
anshbegmirza <anshbegmirza@gmail.com>
2025-09-03 15:19:31 +0530

    Pattern programs

--- CONCEPTS/4.Patterns/patterns.cpp
+++ CONCEPTS/4.Patterns/patterns.cpp
@@ -122,5 +122,186 @@ int main()
   
 //   return 0;
 
+
+ /*
+    reverse integer
+  
+    1 
+    2 1 
+    3 2 1 
+    4 3 2 1   
+    
+  */
+  // int n = 4;
+  
+  // for(int i =0; i<n; i++){//  outerloop
+  //   for(int j=i+1; j > 0; j--){// innerloop
+  //     cout << j <<" ";
+  //   }
+  //   cout << endl;
+  // }
+  
+
+   /* 
+    48. Print Floyd’s triangle.
+    Floyd's triangle
+    1 
+    2 3 
+    4 5 6 
+    7 8 9 10 
+  */
+  
+  // int n=5;
+  // int count = 1;
+  // for (int i =0; i< n; i++){//outer loop
+  
+  // for(int j=0; j< i; j++){//inner loop
+  //   cout << count << " ";
+  //   count++;
+      
+  // }
+  //   cout << endl;
+  // }
+
+  /*
+    reverse chars
+    A 
+    B A 
+    C B A 
+    D C B A 
+  */
+  // int n = 4;
+  
+  // for(int i =0; i<n; i++){//  outerloop
+  //   char val = 'A';
+  //   val = val+i;
+  //   for(int j=i+1; j > 0; j--){// innerloop
+  //     cout << val <<" ";
+  //     val=val-1;
+  //   }
+  //   cout << endl;
+  // }
+
+  /*  
+    Reverse triangle
+    111111111
+     22222222
+      3333333
+       444444
+        55555
+         6666
+          777
+           88
+            9
+  */
+  
+  // int n = 9;
+  
+  // for (int i =0; i<n; i++){//outer loop
+  //   // number of lines
+    
+  //   for(int j =0; j<i; j++){//inner loop #1
+  //   //spaces      
+  //     cout << " ";
+  //   }
+    
+  //    for(int j =0; j<n-i; j++){//inner loop #1
+  //   //spaces      
+  //     cout << (i+1);
+  //   }
+  //   cout << endl; 
+  // }
+
+  /*  
+    PYRAMID PATTERN
+       1
+      121
+     12321
+    1234321
+  */
+  
+  // int n=4;
+  
+  // for(int i=0; i<n; i++){//outer loop
+    
+  //   //spaces
+  //   for(int j=n-i-1; j>0; j--){
+  //     cout << " ";
+  //     // cout << j;
+  //   }
+    
+  //   //nums set1
+  //   for(int j=0; j<i+1; j++){
+  //     cout << j+1;
+  //   }
+    
+    
+  //   //nums set2
+  //   for(int j=i; j > 0; j--){// innerloop
+  //     cout << j;
+  //   }
+    
+    
+  //   cout << endl;
+  // }
+
+   /*  
+    Hollow pyramid
+      *
+     * *
+    *   *
+   *     *
+  *       *
+   *     *
+    *   *
+     * *
+      *
+  */
+  
+  // int n=5;
+  
+  // for(int i=0; i<n; i++){//outer loop
+  //   //top portion only
+  //   //spaces
+  //   for(int j=n-i-1; j>0; j--){
+  //     cout << " ";
+  //   }
+  //     cout << "*";
+    
+  //   if(i!=0){
+  //     // odd spaces
+  //      for(int j=2*i-1; j>0; j--){
+  //     cout << " ";
+  //   }
+      
+  //     cout << "*";
+  //   }
+    
+    
+  //   cout << endl;
+  // }
+  // //bottom loop
+  // for(int i=0; i<n-1; i++){
+    
+  //   // spaces
+  //   for(int j=0; j<i+1; j++){
+  //     cout << " ";
+  //   }
+  //   cout<< "*";
+    
+  //   if(i!=n-2){
+  //   //spaces odd 
+  //   for(int j =2*(n-i)-5; j>0; j--){
+  //     cout << " ";
+  //   }
+    
+  //   cout << "*";
+  //   }  
+    
+    
+  //   cout << endl;
+  // }
+
+
 return 0;
 }
