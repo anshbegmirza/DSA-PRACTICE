5498e509cbdb85145bf8c7afe93736c850e0eb53
anshbegmirza <anshbegmirza@gmail.com>
2025-09-29 11:10:46 +0530

    Weekend dsa revision and practice questions.

---
+++ LEETCODE/Array_Math_Geometry_812. Largest Triangle Area.cpp
@@ -0,0 +1,46 @@
+// Array_Math_Geometry_812. Largest Triangle Area.cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+double largestTriangleArea(vector<vector<int>>& points) {
+        // int area = 0.5 * [x1(y2-y3) + x2(y3-y1) +x3(y1-y2)];
+
+        double maxArea = 0;
+  int n = points.size();
+  
+  for(int i = 0; i<n; i++ ){
+    
+    for(int j = i+1; j<n; j++){
+      
+      for(int k=j+1; k<n; k++){
+        
+        int x1 = points[i][0];
+        int y1 = points[i][1];
+        
+        
+        int x2 = points[j][0];
+        int y2 = points[j][1];
+        
+        
+        int x3 = points[k][0];
+        int y3 = points[k][1];
+      
+      // cout<< x1 << y1 << endl;
+  
+      double area = 0.5 * fabs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+      
+      maxArea = max(maxArea, area);
+        
+      }
+    }
+    
+  }
+  cout << "Max area = " << maxArea << endl;
+    return maxArea;
+    }
+
+    int main(){
+
+        return 0;
+    }
